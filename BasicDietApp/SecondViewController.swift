ca98cdd2a0e6a4962b70d028a9f9c9fe124d0732
sonnyambrogio <sonnyambrogio@gmail.com>
2015-06-24 22:37:37 -0400

    Initial Commit

---
+++ BasicDietApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BasicDietApp
+//
+//  Created by Sonny Ambrogio on 2015-06-24.
+//  Copyright © 2015 Viking Panda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
